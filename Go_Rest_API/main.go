be2a4350ad11591dc1637cde396c4a11ba27459b
João Pedro da Costa Coelho <jp.da.costa.coelho@gmail.com>
2018-12-19 10:55:20 +0000

    Bug correction in updateBook function

--- Go_Rest_API/main.go
+++ Go_Rest_API/main.go
@@ -68,7 +68,7 @@ func updateBook(w http.ResponseWriter, r *http.Request) {
 			books = append(books[:index], books[index+1:]...)
 			var book Book
 			_ = json.NewDecoder(r.Body).Decode(&book)
-			book.ID = strconv.Itoa(rand.Intn(1000000)) //Mock ID - NOT SAFE - Just for example
+			book.ID = params["id"]
 			json.NewEncoder(w).Encode(book)
 			return
 		}
